9318c402e6e273bd198ab3aafd2de5bf6ec464d3
Daniel Zhang <Daniel20482048@outlook.com>
2025-06-14 16:16:12 +0800

    Chapter two codes.

---
+++ chapter_two/hello_world.py
@@ -0,0 +1,14 @@
+# message is a variable
+message = "python crash course"
+print(message.upper())
+
+first_name = "Daniel"
+last_name = "Zhang"
+# f stands for format (the string)
+full_name = f"{first_name} {last_name}"
+print(full_name)
+
+# .rstrip .lstrip .strip: All used for removing blank in the string
+favorite_language = " Swift "
+print(favorite_language)
+print(favorite_language.strip())
---
+++ chapter_two/number.py
@@ -0,0 +1,7 @@
+# _ in number is always being ignored
+number = 100000
+print(number)
+number = 100_000
+print(number)
+# FULL CAPITALIZATION INDICATES CONSTANT
+MAX_CONNECTIONS = 5000
